7c8a52f6a337d77268ec96cf787604c69b07e657
Giovanni Bitonti <67539887+GiovanniBitonti@users.noreply.github.com>
2021-06-08 21:47:26 +0100

    Add files via upload
    initial commit

---
+++ Assets/Js/about.js
@@ -0,0 +1,8 @@
+var controller = new ScrollMagic.Controller();
+
+var animateAuthor = TweenMax.to(".author", 1.5, { opacity: 1, marginTop: 75, delay: 1.5 });
+
+var animateIntro = TweenMax.to(".intro", 1.5, { opacity: 1, marginTop: 50, delay: 2.5 });
+
+var animateSection1Fstop = TweenMax.to("#section1-fstop", 5, { opacity: 1});
+
